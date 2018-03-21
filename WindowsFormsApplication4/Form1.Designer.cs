87fccffa46ce1c0b2926e653edba23306bf5caef
IsraelBerger <37382392+IsraelBerger@users.noreply.github.com>
2018-03-21 20:54:31 +0000

    added calculator

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.butt1 = new System.Windows.Forms.Button();
+            this.butt4 = new System.Windows.Forms.Button();
+            this.butt7 = new System.Windows.Forms.Button();
+            this.butt0 = new System.Windows.Forms.Button();
+            this.buttE = new System.Windows.Forms.Button();
+            this.butt2 = new System.Windows.Forms.Button();
+            this.butt5 = new System.Windows.Forms.Button();
+            this.butt8 = new System.Windows.Forms.Button();
+            this.buttA = new System.Windows.Forms.Button();
+            this.buttT = new System.Windows.Forms.Button();
+            this.butt3 = new System.Windows.Forms.Button();
+            this.butt6 = new System.Windows.Forms.Button();
+            this.butt9 = new System.Windows.Forms.Button();
+            this.buttM = new System.Windows.Forms.Button();
+            this.buttD = new System.Windows.Forms.Button();
+            this.buttClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.TextBox();
+            this.buttDot = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // butt1
+            // 
+            this.butt1.Location = new System.Drawing.Point(13, 63);
+            this.butt1.Name = "butt1";
+            this.butt1.Size = new System.Drawing.Size(75, 23);
+            this.butt1.TabIndex = 0;
+            this.butt1.Text = "1";
+            this.butt1.UseVisualStyleBackColor = true;
+            this.butt1.Click += new System.EventHandler(this.butt1_Click);
+            // 
+            // butt4
+            // 
+            this.butt4.Location = new System.Drawing.Point(13, 103);
+            this.butt4.Name = "butt4";
+            this.butt4.Size = new System.Drawing.Size(75, 23);
+            this.butt4.TabIndex = 1;
+            this.butt4.Text = "4";
+            this.butt4.UseVisualStyleBackColor = true;
+            this.butt4.Click += new System.EventHandler(this.butt4_Click);
+            // 
+            // butt7
+            // 
+            this.butt7.Location = new System.Drawing.Point(13, 133);
+            this.butt7.Name = "butt7";
+            this.butt7.Size = new System.Drawing.Size(75, 23);
+            this.butt7.TabIndex = 2;
+            this.butt7.Text = "7";
+            this.butt7.UseVisualStyleBackColor = true;
+            this.butt7.Click += new System.EventHandler(this.butt7_Click);
+            // 
+            // butt0
+            // 
+            this.butt0.Location = new System.Drawing.Point(13, 163);
+            this.butt0.Name = "butt0";
+            this.butt0.Size = new System.Drawing.Size(75, 23);
+            this.butt0.TabIndex = 3;
+            this.butt0.Text = "0";
+            this.butt0.UseVisualStyleBackColor = true;
+            this.butt0.Click += new System.EventHandler(this.butt0_Click);
+            // 
+            // buttE
+            // 
+            this.buttE.Location = new System.Drawing.Point(13, 226);
+            this.buttE.Name = "buttE";
+            this.buttE.Size = new System.Drawing.Size(75, 23);
+            this.buttE.TabIndex = 4;
+            this.buttE.Text = "=";
+            this.buttE.UseVisualStyleBackColor = true;
+            this.buttE.Click += new System.EventHandler(this.buttE_Click);
+            // 
+            // butt2
+            // 
+            this.butt2.Location = new System.Drawing.Point(105, 62);
+            this.butt2.Name = "butt2";
+            this.butt2.Size = new System.Drawing.Size(75, 23);
+            this.butt2.TabIndex = 5;
+            this.butt2.Text = "2";
+            this.butt2.UseVisualStyleBackColor = true;
+            this.butt2.Click += new System.EventHandler(this.butt2_Click);
+            // 
+            // butt5
+            // 
+            this.butt5.Location = new System.Drawing.Point(105, 102);
+            this.butt5.Name = "butt5";
+            this.butt5.Size = new System.Drawing.Size(75, 23);
+            this.butt5.TabIndex = 6;
+            this.butt5.Text = "5";
+            this.butt5.UseVisualStyleBackColor = true;
+            this.butt5.Click += new System.EventHandler(this.butt5_Click);
+            // 
+            // butt8
+            // 
+            this.butt8.Location = new System.Drawing.Point(105, 133);
+            this.butt8.Name = "butt8";
+            this.butt8.Size = new System.Drawing.Size(75, 23);
+            this.butt8.TabIndex = 7;
+            this.butt8.Text = "8";
+            this.butt8.UseVisualStyleBackColor = true;
+            this.butt8.Click += new System.EventHandler(this.butt8_Click);
+            // 
+            // buttA
+            // 
+            this.buttA.Location = new System.Drawing.Point(105, 162);
+            this.buttA.Name = "buttA";
+            this.buttA.Size = new System.Drawing.Size(75, 23);
+            this.buttA.TabIndex = 8;
+            this.buttA.Text = "+";
+            this.buttA.UseVisualStyleBackColor = true;
+            this.buttA.Click += new System.EventHandler(this.buttA_Click);
+            // 
+            // buttT
+            // 
+            this.buttT.Location = new System.Drawing.Point(105, 192);
+            this.buttT.Name = "buttT";
+            this.buttT.Size = new System.Drawing.Size(75, 23);
+            this.buttT.TabIndex = 9;
+            this.buttT.Text = "*";
+            this.buttT.UseVisualStyleBackColor = true;
+            this.buttT.Click += new System.EventHandler(this.buttT_Click);
+            // 
+            // butt3
+            // 
+            this.butt3.Location = new System.Drawing.Point(207, 62);
+            this.butt3.Name = "butt3";
+            this.butt3.Size = new System.Drawing.Size(75, 23);
+            this.butt3.TabIndex = 10;
+            this.butt3.Text = "3";
+            this.butt3.UseVisualStyleBackColor = true;
+            this.butt3.Click += new System.EventHandler(this.butt3_Click);
+            // 
+            // butt6
+            // 
+            this.butt6.Location = new System.Drawing.Point(207, 102);
+            this.butt6.Name = "butt6";
+            this.butt6.Size = new System.Drawing.Size(75, 23);
+            this.butt6.TabIndex = 11;
+            this.butt6.Text = "6";
+            this.butt6.UseVisualStyleBackColor = true;
+            this.butt6.Click += new System.EventHandler(this.butt6_Click);
+            // 
+            // butt9
+            // 
+            this.butt9.Location = new System.Drawing.Point(207, 132);
+            this.butt9.Name = "butt9";
+            this.butt9.Size = new System.Drawing.Size(75, 23);
+            this.butt9.TabIndex = 12;
+            this.butt9.Text = "9";
+            this.butt9.UseVisualStyleBackColor = true;
+            this.butt9.Click += new System.EventHandler(this.butt9_Click);
+            // 
+            // buttM
+            // 
+            this.buttM.Location = new System.Drawing.Point(207, 162);
+            this.buttM.Name = "buttM";
+            this.buttM.Size = new System.Drawing.Size(75, 23);
+            this.buttM.TabIndex = 13;
+            this.buttM.Text = "-";
+            this.buttM.UseVisualStyleBackColor = true;
+            this.buttM.Click += new System.EventHandler(this.buttM_Click);
+            // 
+            // buttD
+            // 
+            this.buttD.Location = new System.Drawing.Point(207, 192);
+            this.buttD.Name = "buttD";
+            this.buttD.Size = new System.Drawing.Size(75, 23);
+            this.buttD.TabIndex = 14;
+            this.buttD.Text = "/";
+            this.buttD.UseVisualStyleBackColor = true;
+            this.buttD.Click += new System.EventHandler(this.buttD_Click);
+            // 
+            // buttClear
+            // 
+            this.buttClear.Location = new System.Drawing.Point(206, 22);
+            this.buttClear.Name = "buttClear";
+            this.buttClear.Size = new System.Drawing.Size(75, 23);
+            this.buttClear.TabIndex = 16;
+            this.buttClear.Text = "<X";
+            this.buttClear.UseVisualStyleBackColor = true;
+            this.buttClear.Click += new System.EventHandler(this.buttClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(16, 25);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(173, 20);
+            this.label1.TabIndex = 17;
+            // 
+            // buttDot
+            // 
+            this.buttDot.Location = new System.Drawing.Point(13, 191);
+            this.buttDot.Name = "buttDot";
+            this.buttDot.Size = new System.Drawing.Size(75, 23);
+            this.buttDot.TabIndex = 18;
+            this.buttDot.Text = ".DOT";
+            this.buttDot.UseVisualStyleBackColor = true;
+            this.buttDot.Click += new System.EventHandler(this.buttDot_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttDot);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttClear);
+            this.Controls.Add(this.buttD);
+            this.Controls.Add(this.buttM);
+            this.Controls.Add(this.butt9);
+            this.Controls.Add(this.butt6);
+            this.Controls.Add(this.butt3);
+            this.Controls.Add(this.buttT);
+            this.Controls.Add(this.buttA);
+            this.Controls.Add(this.butt8);
+            this.Controls.Add(this.butt5);
+            this.Controls.Add(this.butt2);
+            this.Controls.Add(this.buttE);
+            this.Controls.Add(this.butt0);
+            this.Controls.Add(this.butt7);
+            this.Controls.Add(this.butt4);
+            this.Controls.Add(this.butt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULATOR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button butt1;
+        private System.Windows.Forms.Button butt4;
+        private System.Windows.Forms.Button butt7;
+        private System.Windows.Forms.Button butt0;
+        private System.Windows.Forms.Button buttE;
+        private System.Windows.Forms.Button butt2;
+        private System.Windows.Forms.Button butt5;
+        private System.Windows.Forms.Button butt8;
+        private System.Windows.Forms.Button buttA;
+        private System.Windows.Forms.Button buttT;
+        private System.Windows.Forms.Button butt3;
+        private System.Windows.Forms.Button butt6;
+        private System.Windows.Forms.Button butt9;
+        private System.Windows.Forms.Button buttM;
+        private System.Windows.Forms.Button buttD;
+        private System.Windows.Forms.Button buttClear;
+        private System.Windows.Forms.TextBox label1;
+        private System.Windows.Forms.Button buttDot;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+        List<double> input = new List<double>();
+        List<char> inputCalculate = new List<char>();
+        bool secondclick = true;
+        int i = 0;
+        bool clickedThis = true;
+     
+        private void butt1_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "1";   
+        }
+
+        private void butt2_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "2";
+        }
+
+        private void butt3_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "3";
+        }
+
+        private void butt4_Click(object sender, EventArgs e)
+        {
+            clickedThis =false;
+            label1.Text += "4";
+        }
+
+        private void butt5_Click(object sender, EventArgs e)
+        {
+            label1.Text += "5"; clickedThis = false;
+        }
+
+        private void butt6_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "6";
+        }
+
+        private void butt7_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "7";
+        }
+
+        private void butt8_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "8";
+        }
+
+        private void butt9_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "9";
+        }
+
+        private void butt0_Click(object sender, EventArgs e)
+        {
+            clickedThis = false;
+            label1.Text += "0";
+        }
+ private void buttDot_Click(object sender, EventArgs e)
+        {
+                                clickedThis =false;
+                             label1.Text += ".";
+        }
+        private void buttA_Click(object sender, EventArgs e)
+        {
+            CalculationChoice('+');
+            
+        }
+       
+        private void buttM_Click(object sender, EventArgs e)
+        {
+            CalculationChoice('-');
+        }
+
+        private void buttT_Click(object sender, EventArgs e)
+        {
+            CalculationChoice('*');
+        }
+
+        private void buttD_Click(object sender, EventArgs e)
+        {
+            CalculationChoice('/');
+        }
+
+        private void buttE_Click(object sender, EventArgs e)
+        {
+            try {
+                if (secondclick == true && i != 0)
+                {
+
+                    double c = double.Parse(label1.Text);
+                    input.Insert(i, c);
+                    double result = input[0];
+                    for (int iterate = 0; iterate < i; iterate++)
+                    {
+
+                        switch (inputCalculate[iterate])
+                        {
+                            case '+':
+                                result = result + input[iterate + 1];
+                                break;
+                            case '-':
+                                result = result - input[iterate + 1];
+                                break;
+                            case '*':
+                                result = result * input[iterate + 1];
+                                break;
+                            case '/':
+                                result = result / input[iterate + 1];
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    input.Clear();
+                    inputCalculate.Clear();
+                    i = 0;
+                    label1.Text = result.ToString();
+                    secondclick = false;
+                }
+
+
+            }
+            catch (Exception)
+                     {
+
+                MessageBox.Show("only enter digits");
+            }
+            }
+ private void CalculationChoice(char Chose)
+        {
+            try
+            {
+
+           
+            secondclick = true;
+            if (clickedThis && label1.Text.ToString() != string.Empty)
+            {
+                inputCalculate.Insert(i - 1, Chose);
+            }
+            if (label1.Text.ToString() != string.Empty && label1.Text.ToString()!=".")
+            {
+                double c = double.Parse(label1.Text);
+                input.Insert(i, c);
+
+                inputCalculate.Insert(i, Chose);
+                i++;
+                label1.Text = string.Empty;
+                clickedThis = true;
+            }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("only enter digits");
+                System.Windows.Forms.MessageBox.Show(e.Message);
+            }
+        }
+
+        private void buttClear_Click(object sender, EventArgs e)
+        {
+            input.Clear();
+            inputCalculate.Clear();
+            i = 0;
+            label1.Text = string.Empty;
+            secondclick = true;
+        }
+
+       
+    }
+}
